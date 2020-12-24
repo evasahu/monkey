e9ba443a80da9264d237816c1c4b0ec802d78448
evasahu <74721785+evasahu@users.noreply.github.com>
2020-12-24 11:43:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function preload(){
   monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
   
   bananaImage = loadImage("banana.png");
-  obstaceImage = loadImage("obstacle.png");
+  obstacleImage = loadImage("obstacle.png");
  
 }
 
@@ -28,24 +28,16 @@ function setup() {
   ground.x = ground.width/2;
   console.log(ground.x);
   
+  FoodGroup = new Group();
+  obstaclesGroup = new Group();
   
 }
 
 
 function draw() {
 background(255);
-  
-  if(ground.x < 0){
-    ground.x = ground.width/2;
-  }
-  
-  if(keyDown("space")){
-    monkey.velocityY  = -12;
-  }
-  monkey.velocityY = monkey.velociityY + 0.8;
-  
-  monkey.collide("ground");
-}
+ 
+
 
 stroke("white");
 textSize(20);
@@ -62,10 +54,30 @@ text("survivalTime: " + survivalTime , 100 , 50);
 spawnFood();
 spawnObstacles();
 
+if(obstaclesGroup.isTouching(monkey)){
+  ground.velocityX = 0;
+  ground.velocityY = 0;
+  obstaclesGroup.setVelocityXEach(0);
+  FoodGroup.setVelocityXEach(0);
+  obstaclesGroup.setLifetimeEach(-1);
+  FoodGroup.setLifetimeEach(-1);
+}
 drawSprites();
+   
+  if(ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  if(keyDown("space")){
+    monkey.velocityY  = -12;
+  }
+  monkey.velocityY = monkey.velociityY + 0.8;
+  
+  monkey.collide("ground");
+}
 
 function spawnFood() {
-  //write code here to spawn the clouds
+  //write code here to spawn the food
   if (frameCount % 60 === 0) {
     var food = createSprite(600,120,40,10);
     Food.y = Math.round(random(120,200));
@@ -81,6 +93,27 @@ function spawnFood() {
     ground.depth = ground.depth + 1;
     
     //add each cloud to the group
-    foodGroup.add(Food);
+    foodGroup.add(Fruit);
   }
+}
+
+function spawnObstacles(){
+  //write code here to spawn the obstacles
+  if (frameCount % 60 === 0) {
+    var obstacles = createSprite(600,120,40,10);
+    obstacle.y = Math.round(random(120,200));
+    obstacle.addImage(obstaclesImage);
+    obstacle.scale = 0.5;
+    obstacle.velocityX = -3;
+    
+     //assign lifetime to the variable
+    obstacle.lifetime = 200;
+  
+//adjust the depth
+    obstacle.depth = ground.depth;
+    ground.depth = ground.depth + 1;
+    
+    //add each cloud to the group
+    obstaclesGroup.add(obstacle);
+}
 }
